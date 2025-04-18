a6203ba9900883728a6b3e8886ebd00d80e65264
Inuka Silva <inukasilva85@gmail.com>
2025-04-18 13:16:15 +0530

    book model

---
+++ src/main/java/com/bookstore/Application.java
@@ -0,0 +1,9 @@
+package com.bookstore;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class Application extends jakarta.ws.rs.core.Application {
+
+}
